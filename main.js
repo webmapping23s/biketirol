e1088824af338c614b4b944cff57ab9d5f510175
Klaus Förster <klaus.foerster@uibk.ac.at>
2023-06-01 16:45:41 +0200

    Pulldown für Navigation befüllt

--- main.js
+++ main.js
@@ -67,4 +67,9 @@ let controlElevation = L.control.elevation({
 }).addTo(map);
 controlElevation.load("data/etappe7.gpx");
 
-console.log(ETAPPEN)
+// Pulldown für Navigation
+let pulldown = document.querySelector("#pulldown");
+for (let etappe of ETAPPEN) {
+    console.log(etappe);
+    pulldown.innerHTML += `<option value="${etappe.user}">Etappe ${etappe.nr}: ${etappe.etappe}</option>`
+}
